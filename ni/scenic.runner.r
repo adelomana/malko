bb57ae59e47fd69eed804f3a9d9e6813d89f9724
Adrian de Lomana <alomana@systemsbiology.org>
2018-07-18 10:33:14 -0700

    up to visualization

--- ni/scenic.runner.r
+++ ni/scenic.runner.r
@@ -1,6 +1,6 @@
-# 1. SCENIC Set-up ----
+# 0. SCENIC Set-up ----
 
-# 0.1. SCENIC dependencies
+# 0.1. SCENIC dependencies ---
 source("https://bioconductor.org/biocLite.R")
 #biocLite(c("GENIE3", "RcisTarget", "AUCell"))
 
@@ -15,32 +15,33 @@ packageVersion("GENIE3")
 
 ## Install recommended packages for visualization and parallel processing
 # Recommended to run AUCell:
-#biocLite(c("mixtools"))
 library("mixtools")
 # To visualize the binary matrices and perform t-SNEs:
-#biocLite(c("NMF", "Rtsne", "R2HTML"))
 library("NMF")
 library("Rtsne")
 library("R2HTML")
 # To support paralell execution:
-#biocLite(c("doMC", "doRNG"))
 library("doMC")
 library("doRNG")
+
+library("RCurl")
+library("XML")
+library(data.table)
+library('zoo')
+
 # To visualize in http://scope.aertslab.org
 #install.packages("devtools")
 library(devtools)
 devtools::install_github("aertslab/SCopeLoomR")
 
-library("RCurl")
-library("XML")
-
-# 0.2. Install SCENIC
+# 0.2. Install SCENIC ----
 setwd("/Volumes/omics4tb/alomana/projects/mscni/src/scenic/")
 
 # install.packages("devtools")
 devtools::install_github("aertslab/SCENIC")
+library(SCENIC)
 
-# 0.3. Download databases
+# 0.3. Download databases ----
 ## Download motif databases for RcisTarget (slow: can take >30 min)
 
 #dbFiles <- c("https://resources.aertslab.org/cistarget/databases/homo_sapiens/hg19/refseq_r45/mc9nr/gene_based/hg19-500bp-upstream-7species.mc9nr.feather",
@@ -57,31 +58,29 @@ devtools::install_github("aertslab/SCENIC")
 
 # End SCENIC Set-up
 
-# 1. download test data
+################# work with test data
 
 # (This may take a few minutes)
 #biocLite(c("GEOquery"))
-library(GEOquery)
-geoFile <- getGEOSuppFiles("GSE60361", makeDirectory=FALSE)
-gzFile <- grep("Expression", basename(rownames(geoFile)), value=TRUE)
-txtFile <- gsub(".gz", "", gzFile)
-gunzip(gzFile, destname=txtFile, remove=TRUE)
+#library(GEOquery)
+#geoFile <- getGEOSuppFiles("GSE60361", makeDirectory=FALSE)
+#gzFile <- grep("Expression", basename(rownames(geoFile)), value=TRUE)
+#txtFile <- gsub(".gz", "", gzFile)
+#gunzip(gzFile, destname=txtFile, remove=TRUE)
 
-library(data.table)
-geoData <- fread(txtFile, sep="\t")
-musGeneNames <- unname(unlist(geoData[,1, with=FALSE]))
-musExprMatrix <- as.matrix(geoData[,-1, with=FALSE])
-rm(geoData)
-dim(musExprMatrix)
-rownames(musExprMatrix) <- musGeneNames
-musExprMatrix[1:5,1:4]
+#library(data.table)
+#geoData <- fread(txtFile, sep="\t")
+#musGeneNames <- unname(unlist(geoData[,1, with=FALSE]))
+#musExprMatrix <- as.matrix(geoData[,-1, with=FALSE])
+#rm(geoData)
+#dim(musExprMatrix)
+#rownames(musExprMatrix) <- musGeneNames
+#musExprMatrix[1:5,1:4]
 
-# 2. SCENIC Analysis ----
+# 1. Melanoma data loading and filtering ----
 
-# Load single-cell dataset
-library(data.table)
+# 1.1. Load single-cell dataset ----
 melanomaData=fread("/Volumes/omics4tb/alomana/projects/mscni/data/single.cell.data.txt",sep="\t")
-
 
 dataLength=dim(melanomaData)[1]
 dataWidth=dim(melanomaData)[2]
@@ -96,15 +95,14 @@ TPMs=TPMplusOne-numericalError
 rownames(TPMs)=geneNames
 colnames(TPMs)=cellNames
 
-# calling SCENIC ----
+# 1.2. filter data for SCENIC ----
 dir.create("SCENIC_yapeng")
 setwd("SCENIC_yapeng")
 
-library(SCENIC)
 org="hgnc" # or hgnc, or dmel
 dbDir="/Volumes/omics4tb/alomana/projects/mscni/src/scenic/cisTarget_databases" # RcisTarget databases location
 myDatasetTitle="SCENIC yapeng data analysis" # choose a name for your analysis
-scenicOptions <- initializeScenic(org=org, dbDir=dbDir, datasetTitle=myDatasetTitle, nCores=4) 
+scenicOptions <- initializeScenic(org=org, dbDir=dbDir, datasetTitle=myDatasetTitle, nCores=6) 
 
 dir.create("int")
 
@@ -152,7 +150,7 @@ saveRDS(genesKept, file=getIntName(scenicOptions, "genesKept"))
 TPMs_filtered <- TPMs[genesKept, ]
 rm(TPMs)
 
-## Perform Correlation
+## 1.3. Perform Correlation ---
 corrMat <- cor(t(TPMs_filtered), method="spearman")
 # (Only the rows for TFs will be needed needed):
 # allTFs <- getDbTfs(scenicOptions)
@@ -160,9 +158,7 @@ corrMat <- cor(t(TPMs_filtered), method="spearman")
 saveRDS(corrMat, file=getIntName(scenicOptions, "corrMat"))
 
 
-
-# Run SCENIC --------------------------------------------------------------
-
+# 2. Run SCENIC --------------------------------------------------------------
 
 # Run GENIE3
 # setwd("SCENIC_MouseBrain")
@@ -177,20 +173,54 @@ saveRDS(corrMat, file=getIntName(scenicOptions, "corrMat"))
 # Optional: add log (if it is not logged/normalized already)
 exprMat_filtered <- log2(TPMs_filtered+1) 
 
-# Run GENIE3
+# 2.1. Run GENIE3 ----
 runGenie3(exprMat_filtered, scenicOptions)
 
-# Run GRNBoost
-exportsForGRNBoost(exprMat_filtered, scenicOptions)
-
-# Build and score the GRN (runSCENIC)
+# 2.2. Build and score the GRN (runSCENIC) ----
 scenicOptions <- readRDS("int/scenicOptions.Rds")
 scenicOptions@settings$verbose <- TRUE
-scenicOptions@settings$nCores <- 20
+scenicOptions@settings$nCores <- 6
 scenicOptions@settings$seed <- 123
 
 runSCENIC_1_coexNetwork2modules(scenicOptions)
 runSCENIC_2_createRegulons(scenicOptions)
 runSCENIC_3_scoreCells(scenicOptions, exprMat_filtered)
+
+# 3. visualize results ----
+scenicOptions@settings$seed <- 123 # same seed for all of them
+# Run t-SNE with different settings:
+fileNames <- tsneAUC(scenicOptions, aucType="AUC", nPcs=c(5,15,50), perpl=c(5,15,50))
+fileNames <- tsneAUC(scenicOptions, aucType="AUC", nPcs=c(5,15,50), perpl=c(5,15,50), onlyHighConf=TRUE, filePrefix="int/tSNE_oHC")
+# Plot as pdf (individual files in int/): 
+fileNames <- paste0("int/",grep(".Rds", grep("tSNE_", list.files("int"), value=T), value=T))
+plotTsne_compareSettings(fileNames, scenicOptions, showLegend=FALSE)
+
+par(mfcol=c(3,3))
+fileNames <- paste0("int/",grep(".Rds", grep("tSNE_AUC", list.files("int"), value=T, perl = T), value=T))
+plotTsne_compareSettings(fileNames, scenicOptions, showLegend=FALSE, cex=.5)
+
+# Using only "high-confidence" regulons (normally similar)
+par(mfcol=c(3,3))
+fileNames <- paste0("int/",grep(".Rds", grep("tSNE_oHC_AUC", list.files("int"), value=T, perl = T), value=T))
+plotTsne_compareSettings(fileNames, scenicOptions, showLegend=FALSE, cex=.5)
+
+scenicOptions@settings$defaultTsne$aucType <- "AUC"
+scenicOptions@settings$defaultTsne$dims <- 50
+scenicOptions@settings$defaultTsne$perpl <- 50
+saveRDS(scenicOptions, file="int/scenicOptions.Rds") 
+
+# binarization of regulons
+library('rbokeh')
+logMat <- exprMat_filtered # Better if it is logged/normalized
+aucellApp <- plotTsne_AUCellApp(scenicOptions, logMat)
+savedSelections <- shiny::runApp(aucellApp)
+
+# Save the modified thresholds:
+newThresholds <- savedSelections$thresholds
+scenicOptions@fileNames$int["aucell_thresholds",1] <- "int/newThresholds.Rds"
+saveRDS(newThresholds, file=getIntName(scenicOptions, "aucell_thresholds"))
+saveRDS(scenicOptions, file="int/scenicOptions.Rds") 
+
+# exploring results
 
 
