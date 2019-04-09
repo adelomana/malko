8b257f1d1b1afc322d9de1e1e7e3a3fa983f3a35
Adrian de Lomana <alomana@systemsbiology.org>
2019-04-09 14:41:05 -0700

    pitavastatin only

---
+++ gbm/cell.ranger/gbm.without.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#$ -N without
+#$ -o /proj/omics4tb2/alomana/scratch/messages.GBM.without.o.txt
+#$ -e /proj/omics4tb2/alomana/scratch/messages.GBM.without.e.txt
+#$ -pe smp 40
+#$ -l hostname=baliga2
+#$ -S /bin/bash
+
+cd /users/alomana
+source .bash_profile 
+
+echo "Original paths given my bash_profile..."
+echo $PATH
+echo ""
+
+echo "Adding extra paths for Cell Ranger..."
+source /proj/omics4tb2/alomana/software/cellRanger/cellranger-3.0.2/sourceme.bash
+echo $PATH
+echo ""
+
+echo "Adding path to bcl2fastq..."
+export PATH="/usr/local/bcl2fastq-v2.20.0.422/bin:$PATH"
+echo $PATH
+echo ""
+
+echo "where am i?"
+uname -a
+echo ""
+
+echo "Running count..." 
+cd /proj/omics4tb2/alomana/projects/gbm/results/
+time cellranger aggr --id=without --csv=/proj/omics4tb2/alomana/projects/gbm/data/aggregation.without.csv --normalize=mapped --localcores=40 --localmem=90
+echo "... run completed."
